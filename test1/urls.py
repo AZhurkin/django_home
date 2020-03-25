59805b1b031dd4a3cf69f6d8ed6a5de75cc0860d
Pfkka <igrok.n@mail.ru>
2020-03-25 21:50:00 +0300

    Test1

---
+++ test1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from test1.views import *
+
+urlpatterns = [
+    path('', Test1View.as_view()),
+    path('p2', PageTwo.as_view())
+]
---
+++ test1/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+class Test1View(View):
+
+    def get(self, request):
+        return render(request, 'page1/index.html')
+
+    def post(self, request):
+        html = "<html><body>"
+        for k, v in request.POST.items():
+            html += f"{k}: {v}<br>"
+        html += "</body></html>"
+        return HttpResponse(html)
+
+
+class PageTwo(View):
+
+    def get(self, request):
+        return render(request, 'page2/index.html')
+
+    def post(self, request):
+        html = "<html><body>"
+        for i in request.POST:
+            html += f"{i}<br>"
+        html += "</body></html>"
+        return HttpResponse(html)
